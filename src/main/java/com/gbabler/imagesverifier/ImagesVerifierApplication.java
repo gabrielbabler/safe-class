49cd389ec52e0fbae3de7d30f2ee3e6a97f6bae7
Gabriel Jacob Babler <gabriel.babler@sensedia.local>
2020-07-07 20:11:28 -0300

    base project

---
+++ src/main/java/com/gbabler/imagesverifier/ImagesVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.gbabler.imagesverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbabler/imagesverifier/ImagesVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbabler.imagesverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
